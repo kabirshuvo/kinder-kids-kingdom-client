b1db2c291146a79ae26cdaa12666015e4bdaf2e8
kabirshuvo <kabirshuvo19@gmail.com>
2023-05-18 00:55:54 +0600

    Navbar Updated

--- src/Pages/Shared/Navbar.jsx
+++ src/Pages/Shared/Navbar.jsx
@@ -27,66 +27,48 @@ const Navbar = () => {
               className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
             >
               <li>
-                <a>Item 1</a>
-              </li>
-              <li tabIndex={0}>
-                <a className="justify-between">
-                  Parent
-                  <svg
-                    className="fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                  </svg>
-                </a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
+                <a>Home</a>
               </li>
               <li>
-                <a>Item 3</a>
+                <a>aboutUs</a>
+              </li>
+              <li>
+                <a>allToys</a>
+              </li>
+              <li>
+                <a>myToys</a>
+              </li>
+              <li>
+                <a>addAToy</a>
+              </li>
+              <li>
+                <a>Blog</a>
               </li>
             </ul>
           </div>
-          <Link to='/' className="btn btn-ghost normal-case text-xl">Kinder Kids</Link>
+          <Link to="/" className="btn btn-ghost normal-case text-xl">
+            Kinder Kids
+          </Link>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
             <li>
-              <a>Item 1</a>
-            </li>
-            <li tabIndex={0}>
-              <a>
-                Parent
-                <svg
-                  className="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                </svg>
-              </a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
+              <a>Home</a>
             </li>
             <li>
-              <a>Item 3</a>
+              <a>aboutUs</a>
+            </li>
+            <li>
+              <a>allToys</a>
+            </li>
+            <li tabIndex={0}>
+              <a>myToys</a>
+            </li>
+            <li>
+              <a>addAToy</a>
+            </li>
+            <li>
+              <a>Blogs</a>
             </li>
           </ul>
         </div>
