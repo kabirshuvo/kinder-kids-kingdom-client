11e36e6b469752951301d01ea2c86aaff77a96c3
kabirshuvo <kabirshuvo19@gmail.com>
2023-05-18 03:00:49 +0600

    allToys.json data file created and fetched succesfully in allToys route

--- src/Pages/AllToys/Alltoys.jsx
+++ src/Pages/AllToys/Alltoys.jsx
@@ -1,11 +1,44 @@
-
+import { useEffect, useState } from 'react';
 
 const Alltoys = () => {
-    return (
-        <div>
-            <h3>This is all toyes Page</h3>
+  const [toysData, setToysData] = useState([]);
+
+  useEffect(() => {
+    const fetchToysData = async () => {
+      try {
+        const response = await fetch('/allToys.json');
+        const data = await response.json();
+        setToysData(data);
+      } catch (error) {
+        console.error('Error fetching toy data:', error);
+      }
+    };
+
+    fetchToysData();
+  }, []);
+
+  return (
+    <div>
+      <h3>This is the All Toys Page</h3>
+      {toysData.map((toy) => (
+        <div key={toy.id}>
+          <h4>{toy.name}</h4>
+          <img src={toy.picture} alt={toy.name} />
+          <p>Price: ${toy.price}</p>
+          <p>Rating: {toy.rating}</p>
+          <p>Age Group: {toy.ageGroup}</p>
+          <p>Description: {toy.description}</p>
+          <p>Seller: {toy.sellerName}</p>
+          <p>Category: {toy.category}</p>
+          <p>Subcategory: {toy.subcategory}</p>
+          <p>Available Quantity: {toy.availableQuantity}</p>
+          <p>Shipping Method: {toy.shipping.method}</p>
+          <p>Shipping Time: {toy.shipping.time}</p>
+          <hr />
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default Alltoys;
